1cde581249fadcc8aa1fddb8881f5a67e7fad957
Keith McConnell <komcconnell@gmail.com>
2016-02-11 18:47:18 -0800

    Initial commit from original Marvell code.

---
+++ vendor/marvell-sdk/MV88DE3100_SDK/MV88DE3100_Tools/bsp/common/driver/uart/debug.c
@@ -0,0 +1,23 @@
+#include "debug.h"
+
+#if defined(CONFIG_DISABLE_UART)
+#define DEFAULT_DEBUG_LEVEL             PRN_NONE
+#else
+#ifdef CONFIG_FPGA
+#define	DEFAULT_DEBUG_LEVEL		PRN_RES
+#else
+#define	DEFAULT_DEBUG_LEVEL		PRN_NOTICE
+#endif
+#endif
+
+static int dbg_lvl = DEFAULT_DEBUG_LEVEL;
+
+void set_dbg_level(int lvl)
+{
+	dbg_lvl = lvl;
+}
+
+inline int get_dbg_level()
+{
+	return dbg_lvl;
+}
